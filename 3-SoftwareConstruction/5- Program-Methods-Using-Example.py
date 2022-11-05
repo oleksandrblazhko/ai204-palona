bbc8e72c8078c73a773f3ab2c8f74a723414a4c4
Maria Palona <112876310+Mashimur@users.noreply.github.com>
2022-11-05 03:16:14 +0200

    Create 5- Program-Methods-Using-Example.py

---
+++ 3-SoftwareConstruction/5- Program-Methods-Using-Example.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+  # Приклад роботи з класом Account
+  Matvii.create_Account() # Вводимо дані про користувача
+  Matvii=Account("Matvii", "28.11.2002", 300, "+380969491270", "matvii122@gmail.com") # Створюємо об'єкт Matvii
+  # Приклад роботи з класами User, Database, Kursinfo
+  Matvii.review() # Виводимо інформацію про курс 
