b16e42fb5a596b891ddab58d89303ab098ef428b
Maria Palona <112876310+Mashimur@users.noreply.github.com>
2022-11-05 03:35:48 +0200

    Update 3-Program-Сlasses-Example.py

--- 3-SoftwareConstruction/3-Program-Сlasses-Example.py
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.py
@@ -1,24 +1,33 @@
+# Створення об'єктного типу (клас) Database
 class Database:
     def __init__(self, ddatak, ddatab):
         self.ddataK = ddatak
         self.ddataB = ddatab
 
-
+# Створення об'єктного типу (клас) Kursinfo
 class Kursinfo:
     def __init__(self, kurs_about, kurs_price):
         self.kurs_about = kurs_about
         self.kurs_price = kurs_price
 
-
+# Створення об'єктного типу (клас) Account
 class Account:
-    def __init__(self, aname, abirthday, account_id, aphonenumber, ae_mail):
+    def create_Account(self):
+        aname = input("Введіть ім'я: ")
+        abirthday = input("Введіть дату народження: ")
+        aphonenumber = input("Введіть номер телефону: ")
+        ae_mail = input("Введіть електрону адресу: ")
+        return aname, abirthday, aphonenumber, ae_mail
+        print("Аккаунт створено")
+
+    def __init__(self, account_id, aname, abirthday, aphonenumber, ae_mail):
         self.aname = aname
         self.abirthday = abirthday
         self.account_id = account_id
         self.aphonenumber = aphonenumber
         self.ae_mail = ae_mail
 
-
+# Створення об'єктного типу (клас) User
 class User(Account, Database, Kursinfo):
     def __init__(self, aname, abirthday, account_id, aphonenumber, ae_mail):
         super().__init__(aname, abirthday, account_id, aphonenumber, ae_mail)
@@ -27,12 +36,12 @@ class User(Account, Database, Kursinfo):
         return print("\nНазва курсу: ", self.ddataK, "\nОпис курсу: ", self.kurs_about, "\nЦіна курсу: ",
                      self.kurs_price)
 
-
+# Створення об'єктного типу (клас) Bezpekainfo
 class Bezpekainfo:
     def __init__(self, bdocuments):
         self.bdocuments = bdocuments
 
-
+# Створення об'єктного типу (клас) assigneeIT
 class assigneeIT:
     def __init__(self, aname, awork):
         self.aname = aname
@@ -46,7 +55,7 @@ class assigneeIT:
         kurs_price = input("Введіть вартість курсу: ")
         return print(kurs_price)
 
-
+# Створення об'єктного типу (клас) Administrator
 class Administrator(Database, Bezpekainfo, Account):
     def __init__(self, admin_name, ddatak, ddatab):
         super().__init__(ddatak, ddatab)
